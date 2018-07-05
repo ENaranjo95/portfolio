5e954d41ca0fd0ce8b77f80ba03eddc174b67243
enaranjo95 <ednara1279@gmail.com>
2018-07-05 17:26:30 -0400

    Fix anchor tag issue

--- js/grayscale.js
+++ js/grayscale.js
@@ -28,7 +28,7 @@
 
   // Collapse Navbar
   var navbarCollapse = function() {
-    if ($("#mainNav").offset().top > 500) {
+    if ($("#mainNav").offset().top > 600) {
       $("#mainNav").addClass("navbar-shrink");
     } else {
       $("#mainNav").removeClass("navbar-shrink");
